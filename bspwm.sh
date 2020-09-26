db82d778b19102e2281299ffc85baf8f6f33465d
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-09-25 22:32:21 -0500

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -7,7 +7,6 @@ echo "
 "
 
 #	sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox ranger ntfs-3g --noconfirm
-
 sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom leafpad lightdm lightdm-gtk-greeter firefox nemo dmenu dialog ntfs-3g --noconfirm
 sudo systemctl enable lightdm.service
 clear
@@ -60,10 +59,15 @@ echo "
  sudo cp /usr/share/doc/polybar/config ~/.config/polybar/
  clear
  sudo echo -e " exec bspwm " >  ~/.xinitrc
-# sudo nano ~/.config/bspwm/bspwmrc
+#sudo nano ~/.config/bspwm/bspwmrc
 
 
 #sudo cp bspwmrc ~/.config/bspwm/
 #sudo cp sxhkdrc ~/.config/sxhkd/
 #sudo cp config ~/.config/polybar/
-#sudo cp launch.sh ~/.config/polybar/
+sudo cp launch.sh ~/.config/polybar/
+
+
+sudo chmod +x ~/.config/polybar/launch.sh
+
+
