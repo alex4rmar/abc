7dc6753f6fd6650cf4528201db862e9398202626
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-11-14 21:28:00 -0500

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,58 +1,54 @@
 #!/bin/bash
-
 echo "
 ######################################
                 PACMAN
 ######################################
 "
-
-#	sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox ranger ntfs-3g --noconfirm
-sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom leafpad lightdm lightdm-gtk-greeter firefox nemo dmenu dialog ntfs-3g notepadqq --noconfirm
+#
+sudo pacman -Syy bspwm sxhkd alacritty unzip nitrogen picom leafpad lightdm lightdm-gtk-greeter firefox ranger nemo dmenu dialog ntfs-3g notepadqq --noconfirm
 sudo systemctl enable lightdm.service
 clear
-
+#
 echo "
 ######################################
                 YAY
 ######################################
 "
-
+#
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
 sudo rm -R yay
 clear
-
+#
 echo "
 ######################################
                 POLYBAR
-######################################
-"
+######################################"
+#
 git clone https://aur.archlinux.org/polybar-git.git
 cd polybar-git
 makepkg -si
 cd ..
 sudo rm -R polybar-git
 clear
-
-
+#
 echo "
 ######################################
                 MKDIR
-######################################
-"
+######################################"
+#
 sudo mkdir ~/.config/bspwm
 sudo mkdir ~/.config/sxhkd
 sudo mkdir ~/.config/polybar
 clear
-
-
+#
 echo "
 ######################################
                 COPIAR
-######################################
-"
+######################################"
+#
 sudo cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
 sudo cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
 sudo cp /usr/share/doc/polybar/config ~/.config/polybar/
